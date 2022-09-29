107054ab4e34e5846dee1816e61d2193113256f7
Ashwini Bhosale <68059290+Ashwini126@users.noreply.github.com>
2022-09-29 16:27:05 +0530

    Create test.java

---
+++ test.java
@@ -0,0 +1,29 @@
+classMain {
+
+
+
+  publicstatic void main(String[] args) {
+
+    
+
+    System.out.println("Enter two numbers");
+
+    intfirst = 100;
+
+    intsecond = 200;
+
+    
+
+    System.out.println(first + " " + second);
+
+
+
+    // add two numbers
+
+    intsum = first + second;
+
+    System.out.println("The sum is: " + sum);
+
+  }
+
+}
